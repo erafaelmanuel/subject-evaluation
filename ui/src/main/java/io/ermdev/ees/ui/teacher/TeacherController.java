373a48ba62ce890f7619bf81b4cab99f4c9c6303
erafaelmanuel <erafaelmanuel@gmail.com>
2017-12-31 13:37:58 +0800

    Apply a simple transition on the  side layout.

--- ui/src/main/java/io/ermdev/ees/ui/teacher/TeacherController.java
+++ ui/src/main/java/io/ermdev/ees/ui/teacher/TeacherController.java
@@ -1,12 +1,14 @@
 package io.ermdev.ees.ui.teacher;
 
 import io.ermdev.ees.business.Dimension;
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.ImagePattern;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +17,12 @@ public class TeacherController implements Initializable {
 
     private Dimension dimension;
     private Stage stage;
+
+    @FXML
+    private VBox sideLayout;
+
+    @FXML
+    private Label lbEvaluate;
 
     public TeacherController() {
         dimension = new Dimension();
@@ -47,6 +55,22 @@ public class TeacherController implements Initializable {
     @FXML
     public void onClickedMinimize() {
         stage.setIconified(true);
+    }
+
+    @FXML
+    public void onEnteredSideLayout() {
+        sideLayout.setMaxWidth(130);
+        Timeline timeline = new Timeline();
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(200), new KeyValue(sideLayout.prefWidthProperty(), 130)));
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(200), new KeyValue(lbEvaluate.visibleProperty(), true)));
+        timeline.play();
+    }
+
+    @FXML
+    public void onExitedSideLayout() {
+        sideLayout.setMaxWidth(55);
+        sideLayout.setPrefWidth(55);
+        lbEvaluate.setVisible(false);
     }
 
     protected void setStage(Stage stage) {
