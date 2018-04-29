54632b8a3476582b8b9f5c9bec39a8742e0d170d
erafaelmanuel <erafaelmanuel@gmail.com>
2018-04-30 01:56:22 +0800

    Update

--- src/main/java/io/ermdev/ees/dao/exception/NoResultFoundException.java
+++
@@ -1,8 +0,0 @@
-package io.ermdev.ees.dao.exception;
-
-public class NoResultFoundException extends RuntimeException {
-
-    public NoResultFoundException(final String message) {
-        super(message);
-    }
-}
--- src/main/java/io/ermdev/ees/dao/exception/SubjectNotBelongException.java
+++
@@ -1,8 +0,0 @@
-package io.ermdev.ees.dao.exception;
-
-public class SubjectNotBelongException extends RuntimeException {
-
-    public SubjectNotBelongException(final String message) {
-        super(message);
-    }
-}
